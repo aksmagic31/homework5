e54015aaa28d478a3c9e734e53ec33179c40e440
aksmagic31 <sammyzhang31@gmail.com>
2022-04-23 14:01:10 -0700

    display time

--- Develop/index.js
+++ Develop/index.js
@@ -1,3 +1,10 @@
+
+
+
+var today = moment();
+$("#date").text(today.format("MMM Do, YYYY"));
+
+
 var hours = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"];
 
 function makeTimeblocks(hour, existingTodo = "") {
